a456b1aee1ba92789af978f8a10baef0230a1e45
redoutable95 <83347740+redoutable95@users.noreply.github.com>
2022-11-01 00:01:51 +0500

    create

---
+++ script.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".header__menu");
+const toggle = document.querySelector(".burger-toggle");
+const headerBtn = document.querySelector(".header__button");
+const body = document.querySelector("body");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  toggle.classList.toggle("active");
+  headerBtn.classList.toggle("active");
+  body.classList.toggle("active");
+});
+
+console.log("gesf");
